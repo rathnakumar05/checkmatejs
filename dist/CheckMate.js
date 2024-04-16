38154aaa529402f45688e2a5c77d893a6805fc29
rathnakumar <rathnak010@gmail.com>
2024-04-16 12:12:56 +0530

    Fix min max bug

--- dist/CheckMate.js
+++ dist/CheckMate.js
@@ -1,4 +1,4 @@
-/*! checkmatejs v1.4.2 */
+/*! checkmatejs v1.4.3 */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.CheckMate = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const non_static_checkers = {
@@ -75,10 +75,10 @@ const static_checkers = {
 
     l = Number(l);
     if (v.length === undefined) {
-      return v > l;
+      return v >= l;
     }
 
-    return v.length > l;
+    return v.length >= l;
   },
 
   is_max: function (v, l = null) {
@@ -89,10 +89,10 @@ const static_checkers = {
 
     l = Number(l);
     if (v.length === undefined) {
-      return v < l;
+      return v <= l;
     }
 
-    return v.length < l;
+    return v.length <= l;
   },
 
   is_true: function (v) {
